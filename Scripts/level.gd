6f49d5cd855b871befb6db6e3540bdf3e5a647c1
Drew Wei <drewngui@gmail.com>
2023-01-19 11:02:35 -0800

    Add double-spawn

--- Scripts/level.gd
+++ Scripts/level.gd
@@ -11,10 +11,14 @@ func _ready():
 func _on_MobTimer_timeout():
     # Create a new instance of the Mob scene.
     var mob_left = mob_scene.instance()
+    var mob_right = mob_scene.instance()
     mob_left.position = get_node("MobSpawnLeft").position
+    mob_right.position = get_node("MobSpawnRight").position
 
     # Spawn the mob by adding it to the Main scene.
     add_child(mob_left)
+    add_child(mob_right)
+    SignalBus.emit_signal("mob_spawned")
     SignalBus.emit_signal("mob_spawned")
 
 func increase_difficulty():
